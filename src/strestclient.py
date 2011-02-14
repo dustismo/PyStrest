73ced8f71fbe7b8a471629be7619ba890c01cf33
dustin norlander <dustinn@gmail.com>
2011-02-14 14:14:51 -0800

    initial commit

---
+++ src/strestclient.py
@@ -0,0 +1,245 @@
+import re
+import asyncore, socket
+from Queue import Queue
+from strestutil import STRESTHeaders, STRESTResponse, STRESTRequest
+import zlib
+from threading import RLock
+import strestutil
+
+
+
+class StrestResponseReader(object):
+    
+    def __init__(self, asynch_buffer, response_callback):
+        self.response = None
+        self._decompressor = None
+        self.asynch_buffer = asynch_buffer
+        self.asynch_buffer.read_until('\r\n\r\n', self._header_callback)
+        self.respose_callback = response_callback
+        
+    
+    def _header_callback(self, bytes):
+        print "****** HEADER"
+        print str(bytes)
+        self.response = STRESTResponse()
+        self.response.parse_headers(bytes)
+        self.read_content()
+        
+    def read_content(self):
+        self._decompressor = None
+        if (self.response.headers.get("Content-Encoding") == "gzip"):
+            # Magic parameter makes zlib module understand gzip header
+            # http://stackoverflow.com/questions/1838699/how-can-i-decompress-a-gzip-stream-with-zlib
+            self._decompressor = zlib.decompressobj(16+zlib.MAX_WBITS)
+        if self.response.headers.get("Transfer-Encoding") == "chunked":
+#            self.chunks = []
+#            self.stream.read_until("\r\n", self._on_chunk_length)
+#            TODO: Handle chunked encoding
+            pass
+        elif "Content-Length" in self.response.headers:
+            num_bytes = int(self.response.headers["Content-Length"])
+            self.asynch_buffer.read_bytes(num_bytes, self._content_callback)
+        else:
+            raise Exception("No Content-length or chunked encoding, "
+                            "don't know how to read")
+        
+        
+    def _content_callback(self, bytes):
+        if self._decompressor :
+            bytes = self._decompressor.decompress(bytes)
+        
+        print "******* CONTENT"
+        print str(bytes)
+        
+        self.response.content = bytes
+        self.respose_callback(self.response)
+        
+        self.asynch_buffer.read_until('\r\n\r\n', self._header_callback)
+            
+        
+'''
+    An asynchronous buffer dealing with byte streams
+    currently only one read request is allowed at a time.  
+    
+    We could queue requests, but that seems fairly dangerous.  Will consider 
+    if there is a need.
+    
+    Should be pretty fast, though some optimizations wouldn't hurt.
+'''
+class AsynchBuffer(object):
+    
+    def __init__(self):
+        self.buf = bytearray()
+        self.pointer = 0
+        self.reader = None
+        self.lock = RLock()
+        
+    
+    def _set_reader(self, method, val, callback):
+        with self.lock :
+            if not method :
+                self.reader = None
+                return
+            
+            if self.reader :
+                print str(self.reader)
+                print method, val, callback
+                raise Exception("There is already a reader waiting")
+            else :
+                self.reader = (method, val, callback)
+        
+        # now try to execute it
+        self._attempt_read()
+    
+    def _get_reader(self):
+        with self.lock :
+            return self.reader
+        
+    
+    '''
+        Reads the specified number of bytes from the buffer
+        calls the callback once the bytes are available.
+    '''
+    def read_bytes(self, num_bytes, callback):
+        self._set_reader("read_bytes", num_bytes, callback)
+
+    '''
+        reads until the specified string is encountered. 
+    '''
+    def read_until(self, string, callback):
+        self._set_reader("read_until", string, callback)
+        
+    '''
+        attempts to read the requested data from the buffer.
+        if read_until or read_bytes has not been called, then
+        this does nothing.
+    '''
+    def _attempt_read(self):
+        reader = self._get_reader()
+        if not reader :
+            return    
+        method = reader[0]
+        if method == 'read_until' :
+            self._read_until(reader[1], reader[2])
+        elif method == 'read_bytes' :
+            self._read_bytes(reader[1], reader[2])
+    '''
+        Reads the specified number of bytes
+        executes the callback once that number of bytes is read
+    '''
+    def _read_bytes(self, num_bytes, callback):    
+        if num_bytes < 1 :
+            self._do_callback('', callback)
+            return
+        
+        with self.lock :
+            if len(self.buf) > num_bytes :
+                bytes = self.buf[0:num_bytes]
+                self.buf = self.buf[num_bytes:]
+                self._do_callback(bytes, callback)
+        
+        
+    def _read_until(self, val, callback):
+        with self.lock :
+            index = self.buf.find(val, self.pointer)
+            if index == -1 :
+                # we set the new pointer to be 4 less then the length.
+                # this is in case the last bytes ends with \r\n\r
+                self.pointer = max(len(self.buf)-len(val), 0) 
+            else :
+                bytes = self.buf[0:index]
+                self.buf = self.buf[index+len(val):]
+                self.pointer = 0
+                self._do_callback(bytes, callback)
+        
+    def _do_callback(self, bytes, callback):
+        self._set_reader(None, None, None)
+        #clear any 
+        callback(bytes)
+        
+    
+        
+    def add_bytes(self, bytes):
+        with self.lock :
+            # This might be an atomic op, check on that..
+            self.buf.extend(bytes)
+        
+        self._attempt_read()
+    
+'''
+    Example asynch httpclient
+    http://docs.python.org/library/asyncore.html
+'''
+class StrestClient(asyncore.dispatcher):
+
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect( (host, port) )
+        self.buffer = bytearray()
+        self.callbacks = dict()
+        self.in_buf = AsynchBuffer()
+        self.responses = StrestResponseReader(self.in_buf, self.message_received)
+        
+    def message_received(self, response):
+        print "recieved"
+        callback = self.callbacks.get(response.headers.get_txn_id())
+        if response.headers.get(strestutil.HEADERS.TXN_STATUS, "complete").lower() == "complete" :
+            del self.callbacks[response.headers.get_txn_id()]
+        if callback :
+            callback[0](response)
+
+    def send_request(self, request, response_callback, error_callback=None):
+        request.headers.set_if_absent(strestutil.HEADERS.TXN_ID, strestutil.generate_txn_id())
+        request.headers.set_if_absent(strestutil.HEADERS.TXN_ACCEPT, "multi")
+        self.callbacks[request.headers.get_txn_id()] = (response_callback, error_callback)
+        
+        # Set the content length
+        if "Content-Length" not in request.headers:
+            length = 0
+            if request.content :
+                length = len(request.content)
+            request.headers['Content-Length'] = length
+        
+
+        lines = [request.method + " " + request.uri + " " + strestutil.STREST_VERSION]
+        lines.extend(["%s: %s" % (n, v) for n, v in request.headers.iteritems()])
+        packet = "\r\n".join(lines) + "\r\n\r\n"
+        self.buffer.extend(packet)
+        
+    
+        
+    def handle_connect(self):
+        pass
+
+    def handle_close(self):
+        self.close()
+
+    def handle_read(self):
+        self.in_buf.add_bytes(self.recv(8192))
+        
+
+    def writable(self):
+        return (len(self.buffer) > 0)
+
+    def handle_write(self):
+        sent = self.send(self.buffer)
+        self.buffer = self.buffer[sent:]
+        
+        
+def example_callback(response):
+    print str(response)
+            
+#main app entry point
+if __name__ == "__main__":
+    client = StrestClient('localhost', 8008)
+    request = STRESTRequest('/firehose')
+    
+    client.send_request(request, example_callback)
+    print "entering loop"
+    asyncore.loop()
+    print "loop done"
+    
+    
+
+    
---
+++ src/strestutil.py
@@ -0,0 +1,207 @@
+#
+# Copyright 2009 Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# originally used in the tornado webserver.  modified for use in STREST 
+# Dustin Norlander
+
+import itertools
+import re
+
+"""STREST utility code shared by clients and servers."""
+
+
+
+''' Atomic increment http://29a.ch/2009/2/20/atomic-get-and-increment-in-python '''
+_counter = itertools.count()
+'''
+    atomically generate a new txn-id.  
+'''
+def generate_txn_id():
+    return 'pystrest_' + str(_counter.next())
+    
+class HEADERS:
+    TXN_ID = "Strest-Txn-Id"
+    TXN_ACCEPT = "Strest-Txn-Accept"
+    TXN_STATUS = "Strest-Txn-Status"
+    
+STREST_VERSION = "STREST/0.1"
+class STRESTHeaders(dict):
+    
+    """A dictionary that maintains Http-Header-Case for all keys.
+
+    Supports multiple values per key via a pair of new methods,
+    add() and get_list().  The regular dictionary interface returns a single
+    value per key, with multiple values joined by a comma.
+
+    >>> h = STRESTHeaders({"content-type": "text/html"})
+    >>> h.keys()
+    ['Content-Type']
+    >>> h["Content-Type"]
+    'text/html'
+
+    >>> h.add("Set-Cookie", "A=B")
+    >>> h.add("Set-Cookie", "C=D")
+    >>> h["set-cookie"]
+    'A=B,C=D'
+    >>> h.get_list("set-cookie")
+    ['A=B', 'C=D']
+
+    >>> for (k,v) in sorted(h.get_all()):
+    ...    print '%s: %s' % (k,v)
+    ...
+    Content-Type: text/html
+    Set-Cookie: A=B
+    Set-Cookie: C=D
+    """
+    def __init__(self, *args, **kwargs):
+        # Don't pass args or kwargs to dict.__init__, as it will bypass
+        # our __setitem__
+        dict.__init__(self)
+        self._as_list = {}
+        self.update(*args, **kwargs)
+        
+        
+    def get_txn_id(self):
+        return self[HEADERS.TXN_ID]
+    
+    def set_txn_id(self, id):
+        self[HEADERS.TXN_ID] = id
+    
+    
+    def set_if_absent(self, key, val):
+        if not self.get(key, False) :
+            self[key] = val
+        
+        
+    
+    # new public methods
+
+    def add(self, name, value):
+        """Adds a new value for the given key."""
+        norm_name = STRESTHeaders._normalize_name(name)
+        if norm_name in self:
+            # bypass our override of __setitem__ since it modifies _as_list
+            dict.__setitem__(self, norm_name, self[norm_name] + ',' + value)
+            self._as_list[norm_name].append(value)
+        else:
+            self[norm_name] = value
+
+    def get_list(self, name):
+        """Returns all values for the given header as a list."""
+        norm_name = STRESTHeaders._normalize_name(name)
+        return self._as_list.get(norm_name, [])
+
+    def get_all(self):
+        """Returns an iterable of all (name, value) pairs.
+
+        If a header has multiple values, multiple pairs will be
+        returned with the same name.
+        """
+        for name, list in self._as_list.iteritems():
+            for value in list:
+                yield (name, value)
+
+    def parse_line(self, line):
+        """Updates the dictionary with a single header line.
+
+        >>> h = STRESTHeaders()
+        >>> h.parse_line("Content-Type: text/html")
+        >>> h.get('content-type')
+        'text/html'
+        """
+        name, value = line.split(":", 1)
+        self.add(name, value.strip())
+
+    @classmethod
+    def parse(cls, headers):
+        """Returns a dictionary from HTTP header text.
+
+        >>> h = STRESTHeaders.parse("Content-Type: text/html\\r\\nContent-Length: 42\\r\\n")
+        >>> sorted(h.iteritems())
+        [('Content-Length', '42'), ('Content-Type', 'text/html')]
+        """
+        h = cls()
+        for line in headers.splitlines():
+            if line:
+                h.parse_line(line)
+        return h
+
+    # dict implementation overrides
+
+    def __setitem__(self, name, value):
+        norm_name = STRESTHeaders._normalize_name(name)
+        dict.__setitem__(self, norm_name, value)
+        self._as_list[norm_name] = [value]
+
+    def __getitem__(self, name):
+        return dict.__getitem__(self, STRESTHeaders._normalize_name(name))
+
+    def __delitem__(self, name):
+        norm_name = STRESTHeaders._normalize_name(name)
+        dict.__delitem__(self, norm_name)
+        del self._as_list[norm_name]
+
+    def get(self, name, default=None):
+        return dict.get(self, STRESTHeaders._normalize_name(name), default)
+
+    def update(self, *args, **kwargs):
+        # dict.update bypasses our __setitem__
+        for k, v in dict(*args, **kwargs).iteritems():
+            self[k] = v
+
+
+    
+    _NORMALIZED_HEADER_RE = re.compile(r'^[A-Z0-9][a-z0-9]*(-[A-Z0-9][a-z0-9]*)*$')
+    @staticmethod
+    def _normalize_name(name):
+        """Converts a name to Http-Header-Case.
+
+        >>> STRESTHeaders._normalize_name("coNtent-TYPE")
+        'Content-Type'
+        """
+        if STRESTHeaders._NORMALIZED_HEADER_RE.match(name):
+            return name
+        return "-".join([w.capitalize() for w in name.split("-")])
+
+def _utf8(value):
+    if value is None:
+        return value
+    if isinstance(value, unicode):
+        return value.encode("utf-8")
+    assert isinstance(value, str)
+    return value
+
+class STRESTRequest(object):
+    def __init__(self, uri, method="GET", headers=None, content=None):
+        if headers is None:
+            headers = STRESTHeaders()
+        headers.set_if_absent(HEADERS.TXN_ACCEPT, "single")
+        headers.set_if_absent(HEADERS.TXN_ID, generate_txn_id())        
+        self.uri = _utf8(uri)
+        self.method = method
+        self.headers = headers
+        self.content = content
+       
+class STRESTResponse(object):
+    def __init__(self):
+        self.headers = STRESTHeaders()
+        self.content = None
+             
+    def parse_headers(self, headers):
+        first_line, _, header_data = str(headers).partition("\r\n")
+        match = re.match("STREST/[01].[0-9]+ ([0-9]+) .*", first_line)
+        assert match
+        self.code = int(match.group(1))
+        self.headers = STRESTHeaders.parse(header_data)   
